a111675dce872628cd32d1983cd97f59352b5f59
Dmitry <dsemennyuk@mail.ru>
2019-09-22 16:22:22 +0300

    initial commit

---
+++ ios/Pods/Target Support Files/Pods-safemode_telemetry_sdk-tvOSTests/Pods-safemode_telemetry_sdk-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_safemode_telemetry_sdk_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_safemode_telemetry_sdk_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-safemode_telemetry_sdkTests/Pods-safemode_telemetry_sdkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_safemode_telemetry_sdkTests : NSObject
+@end
+@implementation PodsDummy_Pods_safemode_telemetry_sdkTests
+@end
